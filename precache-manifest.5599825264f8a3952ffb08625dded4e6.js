d6b9f11949e2a40e5d5b28df438d51897d5bb16a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-22 11:23:21 +0000

    Deploy anil152782/react-assessment to github.com/anil152782/react-assessment.git:gh-pages

---
+++ precache-manifest.5599825264f8a3952ffb08625dded4e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ed8ec657c0dea8bd79fa069e6eaa82a",
+    "url": "/react-assessment/index.html"
+  },
+  {
+    "revision": "efcd1941c1036df7c7e5",
+    "url": "/react-assessment/static/css/main.e4cd520b.chunk.css"
+  },
+  {
+    "revision": "7e88850e9aa87b4280da",
+    "url": "/react-assessment/static/js/2.648448c5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-assessment/static/js/2.648448c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efcd1941c1036df7c7e5",
+    "url": "/react-assessment/static/js/main.c4d335bf.chunk.js"
+  },
+  {
+    "revision": "9b0afdffb58f959aea13",
+    "url": "/react-assessment/static/js/runtime-main.bdc98cac.js"
+  }
+]);
